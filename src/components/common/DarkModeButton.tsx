46f19f2f40cc9439e7dcab7472e770c288a55542
rauljariasz <rauljariasz@gmail.com>
2024-07-02 11:30:22 -0400

    cambios en dark and ligth buttons

--- src/components/common/DarkModeButton.tsx
+++ src/components/common/DarkModeButton.tsx
@@ -21,13 +21,13 @@ const DarkModeButton = () => {
   };
   return (
     <button
-      className='inline-flex h-12 w-12 animate-shimmer items-center justify-center rounded-full border border-slate-800 dark:bg-[linear-gradient(110deg,#fff,45%,#e1e1e1e1,55%,#fff)] bg-[length:200%_100%] font-medium text-black transition-colors focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 focus:ring-offset-slate-50 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] dark:border-gray-400 dark:focus:ring-white dark:focus:ring-offset-black fixed left-6 bottom-6 z-[1000]'
+      className='inline-flex h-12 w-12 animate-shimmer items-center justify-center rounded-full border dark:border-slate-800 bg-[linear-gradient(110deg,#fff,45%,#e1e1e1e1,55%,#fff)] bg-[length:200%_100%] font-medium text-black transition-colors focus:outline-none focus:ring-2 dark:focus:ring-black focus:ring-offset-2 dark:focus:ring-offset-slate-50 dark:bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] border-gray-400 focus:ring-white focus:ring-offset-black fixed left-6 bottom-6 z-[1000]'
       onClick={handleClick}
     >
       {darkmode ? (
-        <MdLightMode className='w-6 h-6 text-[#000103]' />
+        <MdLightMode className='w-6 h-6 text-white' />
       ) : (
-        <MdDarkMode className='w-6 h-6 text-white' />
+        <MdDarkMode className='w-6 h-6 text-[#6694b3]' />
       )}
     </button>
   );
